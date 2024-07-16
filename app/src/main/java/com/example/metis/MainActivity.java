86c8e036a8ab82a144ae08baf6414c4730a3d5bc
Inohara <InoSenju@Senju.com>
2024-07-16 19:11:06 +0400

    что?

--- app/src/main/java/com/example/metis/MainActivity.java
+++ app/src/main/java/com/example/metis/MainActivity.java
@@ -2,27 +2,45 @@ package com.example.metis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.hardware.camera2.CameraAccessException;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int CAMERA_REQUEST = 3;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        CameraUtils cum = new CameraUtils(getApplicationContext());
-        try {
-            System.out.println("!!!!!!!" + Arrays.toString(cum.getCameraIds()));
-        } catch (CameraAccessException e) {
-            throw new RuntimeException(e);
-        }
-
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_my_camera);
 
+        this.imageView = this.findViewById(R.id.imageView1);
+        Button photoButton = this.findViewById(R.id.button);
+        photoButton.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
     }
 }
