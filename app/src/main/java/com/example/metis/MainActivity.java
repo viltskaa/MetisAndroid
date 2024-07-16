0c3208d65fda429581307cde31c163a48277319e
Inohara <InoSenju@Senju.com>
2024-07-16 14:44:13 +0400

    ура видим камеры

--- app/src/main/java/com/example/metis/MainActivity.java
+++ app/src/main/java/com/example/metis/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.metis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.camera2.CameraAccessException;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        CameraUtils cum = new CameraUtils(getApplicationContext());
+        try {
+            System.out.println("!!!!!!!" + Arrays.toString(cum.getCameraIds()));
+        } catch (CameraAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
