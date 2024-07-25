661aa21a5056a7b9c78aef8bbcf681bccd7e26a5
ankav <ankavanka731@gmail.com>
2024-07-25 16:48:23 +0400

    scan qr-code

--- app/src/main/java/com/example/metis/MainActivity.java
+++ app/src/main/java/com/example/metis/MainActivity.java
@@ -5,19 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.hardware.camera2.CameraAccessException;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.util.Log;
-import android.view.View;
+import android.view.KeyEvent;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-import java.util.Arrays;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     private static final int CAMERA_REQUEST = 3;
     private ImageView imageView;
+    private TextView textView;
+    private String scannedResult = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,21 +26,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_my_camera);
 
         this.imageView = this.findViewById(R.id.imageView1);
+        this.textView = this.findViewById(R.id.textView);
         Button photoButton = this.findViewById(R.id.button);
-        photoButton.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cameraIntent, CAMERA_REQUEST);
-            }
+        photoButton.setOnClickListener(v -> {
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cameraIntent, CAMERA_REQUEST);
         });
+
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            char pressedKey = (char) event.getUnicodeChar();
+            if (pressedKey == '\n') {
+                textView.setText(scannedResult);
+                scannedResult = "";
+                return true;
+            } else {
+                scannedResult += pressedKey;
+            }
+        }
+
+        return super.dispatchKeyEvent(event);
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            Bitmap photo = (Bitmap) Objects.requireNonNull(data.getExtras()).get("data");
             imageView.setImageBitmap(photo);
         }
     }
